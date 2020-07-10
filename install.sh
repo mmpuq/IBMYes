b31676e66c8ede99e81decbec358cf9767268c0d
mmpuq <44998325+mmpuq@users.noreply.github.com>
2020-07-10 17:52:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ EOF
 
 clone_repo(){
     echo "进行初始化。。。"
-    git clone https://github.com/CCChieh/IBMYes
+    git clone https://github.com/mmpuq/IBMYes
     cd IBMYes
     git submodule update --init --recursive
     cd v2ray-cloudfoundry/v2ray
